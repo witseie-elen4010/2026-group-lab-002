11606f9de4f429f115439040b8c485095c13b582
Sarcastic-Sunflower <sunet110803@gmail.com>
2026-04-23 09:20:41 +0200

    test(database): Acceptance criteria test for primary key and unique fields

---
+++ database/table_acceptance_criteria.sql
@@ -0,0 +1,20 @@
+-- SQLite
+
+SELECT * FROM staff;
+
+--ALTER TABLE staff ADD COLUMN courses JSON;
+
+UPDATE staff 
+SET courses = '["ELEN4010", "ELEN2020", "ELEN3009"]' 
+WHERE staff_number = 'A000356';
+
+SELECT * FROM staff
+WHERE staff_number = 'A000356';
+
+PRAGMA table_info(staff);
+-- Shows pk = 1 for staff_number, which is the primary key of the table.
+
+PRAGMA index_list(staff);
+PRAGMA index_info('sqlite_autoindex_staff_1');
+PRAGMA index_info('sqlite_autoindex_staff_2');
+-- Confirms email and staff number is unique
